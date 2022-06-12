7a4dfa7fa856820ce39675d948d460742683871b
uros <urosstanic25@gmail.com>
2022-06-12 21:54:09 +0100

    implemented 3x3 prewitt

--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@ int filterVer[FILTER_SIZE * FILTER_SIZE] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
 * @param height image height
 **/
 
-void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height)  //TODO obrisati
-{
+void calculate_filter(int *inBuffer, int *outBuffer, int start_width, int start_height, int stop_width, int stop_height,
+                      int full_width) {
     int filter_x[3][3] = {{-1, 0, 1},
                           {-1, 0, 1},
                           {-1, 0, 1}};
@@ -31,8 +31,8 @@ void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height)
                           {0,  0,  0},
                           {1,  1,  1}};
 
-    for (int i = 1; i < width - 1; ++i) {
-        for (int j = 1; j < height - 1; ++j) {
+    for (int i = start_width; i < stop_width; ++i) {
+        for (int j = start_height; j < stop_height; ++j) {
             double color_x = 0;
             double color_y = 0;
             for (int k = 0; k < 3; ++k) {
@@ -40,7 +40,7 @@ void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height)
                     int xn = i + k - 1;
                     int yn = j + l - 1;
 
-                    int index = xn + yn * width;
+                    int index = xn + yn * full_width;
                     color_x += inBuffer[index] * filter_x[k][l];
                     color_y += inBuffer[index] * filter_y[k][l];
 
@@ -48,30 +48,54 @@ void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height)
                 }
 
             }
-            if (sqrt(pow(color_x, 2) + pow(color_y, 2)) >= 128)
-                outBuffer[i + j * width] = 255;
+            if (abs(color_y) + abs(color_x) >= 128)
+                outBuffer[i + j * full_width] = 255;
             else
-                outBuffer[i + j * width] = 0;
-
+                outBuffer[i + j * full_width] = 0;
 
         }
     }
 }
 
+void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height)  //TODO obrisati
+{
+//    int filter_x[3][3] = {{-1, 0, 1},
+//                          {-1, 0, 1},
+//                          {-1, 0, 1}};
+//
+//    int filter_y[3][3] = {{-1, -1, -1},
+//                          {0,  0,  0},
+//                          {1,  1,  1}};
+//
+//    for (int i = 1; i < width - 1; ++i) {
+//        for (int j = 1; j < height - 1; ++j) {
+//            double color_x = 0;
+//            double color_y = 0;
+//            for (int k = 0; k < 3; ++k) {
+//                for (int l = 0; l < 3; ++l) {
+//                    int xn = i + k - 1;
+//                    int yn = j + l - 1;
+//
+//                    int index = xn + yn * width;
+//                    color_x += inBuffer[index] * filter_x[k][l];
+//                    color_y += inBuffer[index] * filter_y[k][l];
+//
+//
+//                }
+//
+//            }
+//            if (abs(color_y) + abs(color_x) >= 128)
+//                outBuffer[i + j * width] = 255;
+//            else
+//                outBuffer[i + j * width] = 0;
+//
+//
+//        }
+//    }
+    calculate_filter(inBuffer,outBuffer,1,1,width-1,height-1,width);
+}
 
 
-//g.run([&] {
-//nextIterParallel(_row, _col, _width
-/// 2); });
-//g.run([&] {
-//nextIterParallel(_row
-//+ _width / 2, _col, _width / 2); });
-//g.run([&] {
-//nextIterParallel(_row, _col
-//+ _width / 2, _width / 2); });
-//g.run([&] {
-//nextIterParallel(_row
-//+ _width / 2, _col + _width / 2, _width / 2); });
 
 /**
 * @brief Parallel version of edge detection algorithm implementation using Prewitt operator
@@ -81,19 +105,36 @@ void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height)
 * @param width image width
 * @param height image height
 */
-void filter_parallel_prewitt(int *inBuffer, int *outBuffer, int width, int height, int row, int col) {
-//    tbb::task_group g;
-//    if ((width) > THRESHOLD) {
-//        g.run([&] { filter_parallel_prewitt(inBuffer, outBuffer, width / 2, height / 2, row, col); });
-//        g.run([&] { filter_parallel_prewitt(inBuffer, outBuffer, width / 2, height / 2, row + width / 2, col); });
-//        g.run([&] { filter_parallel_prewitt(inBuffer, outBuffer, width / 2, height / 2, row, col + height / 2); });
-//        g.run([&] {
-//            filter_parallel_prewitt(inBuffer, outBuffer, width / 2, height / 2, row + width / 2, col + height / 2);
-//        });
-//        g.wait();
-//    } else {
-//        calculate_filter(inBuffer, outBuffer, width / 2, height / 2, row, col);
-//    }
+void filter_parallel_prewitt(int *inBuffer, int *outBuffer, int start_width, int start_height, int stop_width,
+                             int stop_height, int full_width) {
+    tbb::task_group g;
+    int middle_width = start_width + (stop_width - start_width) / 2;
+    int middle_height = start_height + (stop_height - start_height) / 2;
+    if ((stop_height - start_height) > THRESHOLD) {
+        // 1
+        g.run([=]() {
+            filter_parallel_prewitt(inBuffer, outBuffer, start_width, start_height, middle_width, middle_height,
+                                    full_width);
+        });
+        // 2
+        g.run([=]() {
+            filter_parallel_prewitt(inBuffer, outBuffer, middle_width, start_height, stop_width, middle_height,
+                                    full_width);
+        });
+        // 3
+        g.run([=]() {
+            filter_parallel_prewitt(inBuffer, outBuffer, start_width, middle_height,
+                                    middle_width, stop_height, full_width);
+        });
+        // 4
+        g.run([=]() {
+            filter_parallel_prewitt(inBuffer, outBuffer, middle_width, middle_height, stop_width, stop_height,
+                                    full_width);
+        });
+        g.wait();
+    } else {
+        calculate_filter(inBuffer, outBuffer, start_width, start_height, stop_width, stop_height, full_width);
+    }
 }
 
 /**
@@ -143,7 +184,7 @@ void run_test_nr(int testNr, BitmapRawConverter *ioFile, char *outFileName, int 
             break;
         case 2:
             cout << "Running parallel version of edge detection using Prewitt operator" << endl;
-            filter_parallel_prewitt(ioFile->getBuffer(), outBuffer, width - 1, height - 1, 0, 0);
+            filter_parallel_prewitt(ioFile->getBuffer(), outBuffer, 1, 1, width-1, height-1, width);
             break;
         case 3:
             cout << "Running serial version of edge detection" << endl;
@@ -158,9 +199,8 @@ void run_test_nr(int testNr, BitmapRawConverter *ioFile, char *outFileName, int 
             break;
     }
     // TODO: end measure and display time
-    cout << "OUTFILE\n";
     ioFile->setBuffer(outBuffer);
-    ioFile->pixelsToBitmap("../color1.bmp");
+    ioFile->pixelsToBitmap(outFileName);
 }
 
 int main(int argc, char *argv[]) {
@@ -191,11 +231,11 @@ int main(int argc, char *argv[]) {
     memset(outBufferParallelEdge, 0x0, width * height * sizeof(int));
 
     // serial version Prewitt
-    run_test_nr(1, &outputFileSerialPrewitt, argv[2], outBufferSerialPrewitt, width, height);
+    run_test_nr(1, &outputFileSerialPrewitt, "../color1.bmp", outBufferSerialPrewitt, width, height);
 
     // parallel version Prewitt
-//    run_test_nr(2, &outputFileParallelPrewitt, argv[3], outBufferParallelPrewitt, width, height);
-//
+    run_test_nr(2, &outputFileParallelPrewitt, "../color2.bmp", outBufferParallelPrewitt, width, height);
+
 //    // serial version special
 //    run_test_nr(3, &outputFileSerialEdge, argv[4], outBufferSerialEdge, width, height);
 //
